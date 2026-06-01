fe830e81dca13598436575102a1142c9c84fbfb3
Soham-Suryvanshi <soham.suryvanshi19@gmail.com>
2026-06-01 13:53:59 +0530

    correction in bubble sort logic

--- script.js
+++ script.js
@@ -32,22 +32,24 @@ function sleep(ms){
 }
 
 
-async function bublesort() {
-    for(let i=0;i<arr.length;i++){
-        for(let j=i;j<arr.length;j++)
-            {if(arr[i]>arr[j]){
-                [arr[i],arr[j]]=[arr[j],arr[i]];
+async function bubbleSort() {
+    for(let i=0;i<arr.length-1;i++) {
+        for(let j=0;j<arr.length-i-1;j++) {
+
+            if(arr[j] > arr[j+1]) {
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
                 renderBars();
-                await sleep (150);}
+                await sleep(150);
             }
-        
+        }
     }
-    
 }
+    
+
     const genbut=document.querySelector("#generate-button")
 
         genbut.addEventListener("click", generateBars);
 
     
     const bubsort=document.querySelector("#bubble-sort");
-        bubsort.addEventListener("click",bublesort);
+        bubsort.addEventListener("click",bubbleSort);
