402f241a8415534913aefccbba5bc86e31968f95
Soham-Suryvanshi <soham.suryvanshi19@gmail.com>
2026-06-01 11:53:16 +0530

    Seprated Render Function and showing values on bar

--- script.js
+++ script.js
@@ -4,21 +4,25 @@ let arr=[];
 function generateBars(){
     arr=[];
     for(let i=0;i<10;i++){
-    arr.push(Math.floor(Math.random()*200)+1);
-}
+        arr.push(Math.floor(Math.random()*200)+1);
+    }
+    renderBars();
 
-const barsContainer = document.querySelector(".bars");
-barsContainer.innerHTML=" ";
-
-for(let i=0;i<10;i++){
-   const bar=document.createElement("div");
-bar.classList.add("bar");
-let value = arr[i];
-bar.style.height= value + "px";
-barsContainer.appendChild(bar);
 }
-}
+function renderBars(){
+    const barsContainer = document.querySelector(".bars");
+    barsContainer.innerHTML="";
 
+    for(let i=0;i<arr.length;i++){
+        const bar=document.createElement("div");
+        bar.classList.add("bar");
+        let value = arr[i];
+        bar.style.height= value + "px";
+        bar.textContent=arr[i];
+        barsContainer.appendChild(bar);
+        
+    }
+}
 const genbut=document.querySelector("#generate-button")
 
 genbut.addEventListener("click", generateBars);
